30848aa2bed141b2e49abb8cfc79bc07df6c20e2
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-04-22 10:46:59 +0900

    use const and for ... of
    [BREAKING] drop support for Node.js < 4

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 'use strict';
 
-var resolve = require('path').resolve;
+const resolve = require('path').resolve;
 
-var EXACT_KEY = 'npm_config_cache';
-
-function isNpmConfigCacheKey(key) {
-  return key.toLowerCase() === EXACT_KEY;
-}
+const EXACT_KEY = 'npm_config_cache';
 
 module.exports = function npmCacheEnv() {
-  var envs = Object.keys(process.env);
+  const keys = Object.keys(process.env);
 
   // https://github.com/npm/npm/commit/f3087cc58c903d9a70275be805ebaf0eadbcbe1b
   // > inside npm-scripts npm will set it's own environment variables and
   // > Node will prefer those lowercase versions over any uppercase ones that you might set.
-  if (envs.indexOf('npm_lifecycle_event') !== -1) {
+  if (keys.indexOf('npm_lifecycle_event') !== -1) {
     return resolve(process.env[EXACT_KEY]);
   }
 
-  var cacheEnvs = envs.filter(isNpmConfigCacheKey);
-
-  if (cacheEnvs.length === 0) {
-    return null;
+  // Use the last key by reverse iteration to follow the npm CLI behavior:
+  // https://github.com/npm/npm/blob/v4.5.0/lib/config/core.js#L347-L358
+  for (const key of keys.reverse()) {
+    if (key.toLowerCase() === EXACT_KEY) {
+      return resolve(process.env[key]);
+    }
   }
 
-  // Uses the last key to follow the npm CLI behavior:
-  // https://github.com/npm/npm/blob/v4.5.0/lib/config/core.js#L347-L358
-  return resolve(process.env[cacheEnvs[cacheEnvs.length - 1]]);
+  return null;
 };
