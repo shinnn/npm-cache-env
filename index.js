ba94b179371295099e27f714af6ee8a39857455c
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-07-02 19:46:57 +0900

    use destructuring assignment and Array#includes
    [BREAKING] drop support for Node.js < 6

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const resolve = require('path').resolve;
+const {resolve} = require('path');
 
 const EXACT_KEY = 'npm_config_cache';
 
@@ -10,12 +10,12 @@ module.exports = function npmCacheEnv() {
   // https://github.com/npm/npm/commit/f3087cc58c903d9a70275be805ebaf0eadbcbe1b
   // > inside npm-scripts npm will set it's own environment variables and
   // > Node will prefer those lowercase versions over any uppercase ones that you might set.
-  if (keys.indexOf('npm_lifecycle_event') !== -1) {
+  if (keys.includes('npm_lifecycle_event')) {
     return resolve(process.env[EXACT_KEY]);
   }
 
   // Use the last key by reverse iteration to follow the npm CLI behavior:
-  // https://github.com/npm/npm/blob/v4.5.0/lib/config/core.js#L347-L358
+  // https://github.com/npm/npm/blob/v5.0.4/lib/config/core.js#L347-L358
   for (const key of keys.reverse()) {
     if (key.toLowerCase() === EXACT_KEY) {
       return resolve(process.env[key]);
